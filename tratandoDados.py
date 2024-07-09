7c46030ab7293f5ead676a55db088b5a01956ce9
RafaBonach <rafa.bonach@gmail.com>
2024-07-09 20:47:48 -0300

    Resolvendo a primeira questão de do projeto

--- tratandoDados.py
+++ tratandoDados.py
@@ -1,4 +1,33 @@
 import pandas as pd
 
 data = pd.read_csv('./Drugs.csv')
-print(data)
+data_pre = data.copy()
+del data_pre['ID']
+del data_pre['Income (USD)']
+data_pre['Impulsive'] = pd.to_numeric(data_pre['Impulsive'], errors='coerce')
+# Alterando os valores nulos para 0
+data_pre_fill = data_pre.fillna(value={
+    'Nscore': 0,
+    'Escore': 0,
+    'Oscore':0,
+    'AScore':0,
+    'Cscore':0,
+    'Impulsive':0,
+    'SS':0
+})
+#print(data_pre_fill) # valores que antes eram NaN agora são 0
+data_pre_fill = data_pre.fillna(value={
+    'Nscore': 0,
+    'Escore': 0,
+    'Oscore':0,
+    'AScore':0,
+    'Cscore':0,
+    'Impulsive':0,
+    'SS':0
+})
+#remove no prprio dataframe, todas as linhas com valores NaN em quaisquer colunas/atributos
+data_pre_fill.dropna(inplace=True)
+#print(data_pre.info())
+print(data_pre_fill.info())
+data_pre_fill.to_csv('./Drugs_preprocessado.csv', index=False)
+
